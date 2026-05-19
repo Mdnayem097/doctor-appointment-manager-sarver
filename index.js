ddcda92d5184b02d160ac4f6ddca9f6123fd35e9
Mdnayem097 <mdneloy097@gmail.com>
2026-05-19 19:42:02 +0600

    fix get error

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
+
 
 dotenv.config()
 const app = express()
@@ -9,7 +10,7 @@ const port = process.env.PORT || 5000;
 const uri = process.env.MONGODB_URI
 
 app.use(cors())
-app.use(express())
+app.use(express.json())
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -30,6 +31,16 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result)
     })
+
+    app.get('/all-appointment/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+
+      const query = { _id: new ObjectId(id) };
+      const user = await doctorData.findOne(query);
+
+      res.send(user);
+    });
 
 
     await client.db("admin").command({ ping: 1 });
